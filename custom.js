bcdeea46eaa5d392dc86a0ee9060801accf70dce
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2021-02-14 11:20:06 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,3 @@
-
 var resizeId;
 var openedSidePanel;
 var bodyHasResponsiveNavigation = 0;
@@ -123,34 +122,6 @@ $(document).ready(function($) {
         });
     }
 
-    if ($('.video-popup').length > 0) {
-        $('.video-popup').magnificPopup({
-            type:'iframe',
-            removalDelay: 300,
-            mainClass: 'mfp-fade',
-            overflowY: 'scroll',
-            iframe: {
-                markup: '<div class="mfp-iframe-scaler">'+
-                    '<div class="mfp-close"></div>'+
-                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                    '</div>',
-                patterns: {
-                    youtube: {
-                        index: 'youtube.com/',
-                        id: 'v=',
-                        src: 'http://youtube.com/embed/%id%?autoplay=1'
-                    },
-                    vimeo: {
-                        index: 'vimeo.com/',
-                        id: '/',
-                        src: 'http://player.vimeo.com/video/%id%?autoplay=1'
-                    }
-                },
-                srcAction: 'iframe_src'
-            }
-        });
-    }
-
     if( $(".count-down").length ){
         var year = parseInt( $(".count-down").attr("data-countdown-year"), 10 );
         var month = parseInt( $(".count-down").attr("data-countdown-month"), 10 ) - 1;
@@ -161,11 +132,6 @@ $(document).ready(function($) {
         //$(".count-down").countdown({until: new Date(year, month, day), padZeroes: true});
     }
 
-
-
-    $(".bg-transfer").each(function() {
-        $(this).css("background-image", "url("+ $(this).find("img").attr("src") +")" );
-    });
 
     if( $("body").hasClass("nav-btn-only") ){
         bodyHasResponsiveNavigation = 1;
@@ -193,64 +159,6 @@ $(window).resize(function(){
 function doneResizing(){
     responsiveNavigation();
     $(".tse-scrollable").TrackpadScrollEmulator("recalculate");
-}
-
-
-// 8. Owl-Carousel //
-function initializeOwl(){
-    if( $(".owl-carousel").length ){
-        $(".owl-carousel").each(function() {
-
-            var items = parseInt( $(this).attr("data-owl-items"), 10);
-            if( !items ) items = 1;
-
-            var nav = parseInt( $(this).attr("data-owl-nav"), 2);
-            if( !nav ) nav = 0;
-
-            var dots = parseInt( $(this).attr("data-owl-dots"), 2);
-            if( !dots ) dots = 0;
-
-            var center = parseInt( $(this).attr("data-owl-center"), 2);
-            if( !center ) center = 0;
-
-            var loop = parseInt( $(this).attr("data-owl-loop"), 2);
-            if( !loop ) loop = 0;
-
-            var margin = parseInt( $(this).attr("data-owl-margin"), 2);
-            if( !margin ) margin = 0;
-
-            var autoWidth = parseInt( $(this).attr("data-owl-auto-width"), 2);
-            if( !autoWidth ) autoWidth = 0;
-
-            var navContainer = $(this).attr("data-owl-nav-container");
-            if( !navContainer ) navContainer = 0;
-
-            var autoplay = $(this).attr("data-owl-autoplay");
-            if( !autoplay ) autoplay = 0;
-
-            var fadeOut = $(this).attr("data-owl-fadeout");
-            if( !fadeOut ) fadeOut = 0;
-            else fadeOut = "fadeOut";
-
-            $(this).owlCarousel({
-                navContainer: navContainer,
-                animateOut: fadeOut,
-                autoplaySpeed: 2000,
-                autoplay: autoplay,
-                autoheight: 1,
-                center: center,
-                loop: loop,
-                margin: margin,
-                autoWidth: autoWidth,
-                items: items,
-                nav: nav,
-                dots: dots,
-                autoHeight: true,
-                navText: []
-            });
-        });
-
-    }
 }
 
 function responsiveNavigation(){
